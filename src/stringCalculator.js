a7984cb5c2438846c042f5ad2663cbe2773153e2
Charlotte Fereday <cbrfereday@gmail.com>
2017-08-08 19:00:59 +0100

    Updates add function now returns an integer for string numbers, invokes convert function

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -5,7 +5,28 @@ var stringCalculator = function() {};
 stringCalculator.prototype.add = function(string) {
   if (string === "") {
     return 0;
-  } else {
+  } else if (string.length > 1) {
+    var splitString = []
+    var result = 0;
+    splitString = string.split(",");
+    splitString = this.convert(splitString);
+    for (var i in splitString) {
+      splitString.reduce(function(sum, value) {
+        result =  sum + value;
+      });
+    }
+    return result;
+  }
+  else {
     return parseInt(string);
   }
+};
+
+stringCalculator.prototype.convert = function(arrayOfStrings) {
+  var arrayofIntegers = [];
+  for (var string of arrayOfStrings) {
+    var converted = parseInt(string);
+    arrayofIntegers.push(converted);
+  }
+  return arrayofIntegers;
 };
--- test/spec/stringCalculatorSpec.js
+++ test/spec/stringCalculatorSpec.js
@@ -1,14 +1,24 @@
 'use strict';
 
 describe('StringCalculator', function() {
+  var calculator = new stringCalculator;
+
   it("an empty string returns 0", function() {
-    var calculator = new stringCalculator;
     expect(calculator.add("")).toEqual(0);
   });
 
   it("turns a string number into an integer", function() {
-    var calculator = new stringCalculator;
     expect(calculator.add("1")).toEqual(1);
     expect(calculator.add("2")).toEqual(2);
+  });
+
+  it("returns the sum of two string numbers", function() {
+    expect(calculator.add("1,2")).toEqual(3);
+    expect(calculator.add("3,4")).toEqual(7);
+  });
+
+  it("turns an array of string numbers into integers", function() {
+    expect(calculator.convert(["1","2"])).toEqual([1, 2]);
+    expect(calculator.convert(["3","4"])).toEqual([3, 4]);
   });
 });
