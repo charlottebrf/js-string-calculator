13ae2a86f06fba0e500a6b4f7b6101626ad7b028
Charlotte Fereday <cbrfereday@gmail.com>
2017-08-09 09:40:13 +0100

    Minor refactor to test descriptions to add clarity

--- test/spec/stringCalculatorSpec.js
+++ test/spec/stringCalculatorSpec.js
@@ -3,39 +3,44 @@
 describe('StringCalculator', function() {
   var calculator = new stringCalculator;
 
-  it("an empty string returns 0", function() {
-    expect(calculator.add("")).toEqual(0);
+  describe('#add', function() {
+    it("an empty string returns 0", function() {
+      expect(calculator.add("")).toEqual(0);
+    });
+
+    it("turns a string number into an integer", function() {
+      expect(calculator.add("1")).toEqual(1);
+      expect(calculator.add("2")).toEqual(2);
+    });
+
+    it("returns the sum of multiple string numbers separated by a comma", function() {
+      expect(calculator.add("1,2")).toEqual(3);
+      expect(calculator.add("3,4,5")).toEqual(12);
+    });
+
+    it("returns the sum of multiple string numbers separated by a newline", function() {
+      expect(calculator.add("1\n2")).toEqual(3);
+      expect(calculator.add("1\n2,3")).toEqual(6);
+    })
+
+    it("returns the sum of multiple numbers separated by any punctuation delineator", function() {
+      expect(calculator.add(";\n1;2")).toEqual(3);
+      expect(calculator.add("3\n1;2")).toEqual(6);
+      expect(calculator.add("//;\n1;2")).toEqual(3);
+    })
   });
 
-  it("turns a string number into an integer", function() {
-    expect(calculator.add("1")).toEqual(1);
-    expect(calculator.add("2")).toEqual(2);
+  describe('#convert', function() {
+    it("turns an array of string numbers into integers", function() {
+      expect(calculator.convert(["1","2"])).toEqual([1, 2]);
+      expect(calculator.convert(["3","4"])).toEqual([3, 4]);
+    });
   });
 
-  it("returns the sum of multiple string numbers separated by a comma", function() {
-    expect(calculator.add("1,2")).toEqual(3);
-    expect(calculator.add("3,4,5")).toEqual(12);
-  });
-
-  it("returns the sum of multiple string numbers separated by a newline", function() {
-    expect(calculator.add("1\n2")).toEqual(3);
-    expect(calculator.add("1\n2,3")).toEqual(6);
+  describe('#integerCheck', function() {
+    it("deletes any elements that are not integers", function() {
+      var result = calculator.convert([";","\n","1", ";", "2"])
+      expect(calculator.integerCheck([";","\n",1,";", 2])).toEqual([1, 2]);
+    });
   })
-
-  it("returns the sum of multiple numbers separated by any punctuation delineator", function() {
-    expect(calculator.add(";\n1;2")).toEqual(3);
-    expect(calculator.add("3\n1;2")).toEqual(6);
-    expect(calculator.add("//;\n1;2")).toEqual(3);
-  })
-
-  it("turns an array of string numbers into integers", function() {
-    expect(calculator.convert(["1","2"])).toEqual([1, 2]);
-    expect(calculator.convert(["3","4"])).toEqual([3, 4]);
-  });
-
-  it("deletes any elements that are not integers", function() {
-    var result = calculator.convert([";","\n","1", ";", "2"])
-    expect(calculator.integerCheck([";","\n",1,";", 2])).toEqual([1, 2]);
-  });
-
 });
